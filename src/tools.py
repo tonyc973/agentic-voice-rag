447df68f66a3e0a1ba2c5aa68bb22779f7c9a8ee
tonyc973 <anastasiac2020@gmail.com>
2025-12-01 19:37:43 +0200

    Update tools.py

--- src/tools.py
+++ src/tools.py
@@ -3,11 +3,13 @@ from crewai.tools import BaseTool
 from typing import Type
 from pydantic import BaseModel, Field
 
-# --- Tool Input Schema ---
+# Tool Input Schema
 class SearchPdfToolInput(BaseModel):
     query: str = Field(..., description="The query string to search in the PDF")
 
-# --- Tool Definition ---
+# Tool Definition 
+# go to https://docs.crewai.com/en/tools/ to find out more about creating tools in crewai 
+# sky is the limit
 class SearchPdfTool(BaseTool):
     name: str = "SearchPdfTool"
     description: str = "Tool used to search information in a PDF given a query"
